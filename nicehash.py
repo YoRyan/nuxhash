8764f49c22963c5d88cd7752f1b37f098e9aba37
Ryan A Young <rayoung@utexas.edu>
2018-02-10 03:25:27 -0600

    Implement NiceHash algorithm pay information.

--- nicehash.py
+++ nicehash.py
@@ -7,19 +7,36 @@ TIMEOUT = 20
 def api_call(method, params):
     get_data = { 'method': method }
     get_data.update(params)
+
     request = urllib2.urlopen('https://api.nicehash.com/api?%s' %
                               urllib.urlencode(get_data), timeout=TIMEOUT)
+
     return json.load(request, 'ascii')
 
 def algorithm_payrates():
     response = api_call('simplemultialgo.info', {})
+
     algorithms = response['result']['simplemultialgo']
     payrates = dict([(a['name'], float(a['paying'])) for a in algorithms])
     ports = dict([(a['name'], int(a['port'])) for a in algorithms])
+
     return payrates, ports
 
 def unpaid_balance(address):
     response = api_call('stats.provider', { 'addr': address })
     balances = response['result']['stats']
     return sum([float(b['balance']) for b in balances])
+
+def simplemultialgo_info(nx_settings):
+    response = api_call('simplemultialgo.info', [])
+
+    algorithms_info = response['result']['simplemultialgo']
+    mbtc_per_hash = dict([(a['name'], float(a['paying'])*1e-11)
+                          for a in algorithms_info])
+    stratums = dict([(a['name'], ('%s.%s.nicehash.com:%d' %
+                                  (a['name'], nx_settings['nicehash']['region'],
+                                   a['port'])))
+                     for a in algorithms_info])
+
+    return mbtc_per_hash, stratums
 
