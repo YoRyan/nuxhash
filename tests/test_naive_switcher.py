ffb1679ff19b7dea1a0a5863d9d46314bf52b17e
Ryan Young <rayoung@utexas.edu>
2018-09-20 14:17:03 -0700

    Naive profit switcher test code

---
+++ tests/test_naive_switcher.py
@@ -0,0 +1,58 @@
+from nuxhash.miners.excavator import Excavator
+from nuxhash.switching.naive import NaiveSwitcher
+import nuxhash.settings
+import tests
+
+from pathlib2 import Path
+from unittest import main, TestCase
+
+class TestNaiveSwitcher(TestCase):
+    def setUp(self):
+        settings = nuxhash.settings.DEFAULT_SETTINGS
+        settings['switching']['threshold'] = 0.5
+
+        self.devices = tests.get_test_devices()
+        self.benchmarks = tests.get_test_benchmarks()
+        self.miner = Excavator(Path('/'), settings)
+        self.equihash = next(a for a in self.miner.algorithms
+                             if a.algorithms == ['equihash'])
+        self.neoscrypt = next(a for a in self.miner.algorithms
+                              if a.algorithms == ['neoscrypt'])
+
+        self.switcher = NaiveSwitcher(settings)
+        self.switcher.reset()
+
+    def test_most_profitable(self):
+        device = self.devices[0]
+
+        equihash_revenue = 4.0*self.benchmarks[device]['excavator_equihash'][0]
+        neoscrypt_revenue = 2.0*self.benchmarks[device]['excavator_neoscrypt'][0]
+        revenues = { device: { self.equihash: equihash_revenue,
+                               self.neoscrypt: neoscrypt_revenue } }
+        decision = self.switcher.decide(revenues, None)
+
+        self.assertEqual(decision[device], self.neoscrypt)
+
+    def test_below_threshold(self):
+        device = self.devices[0]
+
+        self.switcher.decide({ device: { self.equihash: 2.0,
+                                         self.neoscrypt: 1.0 } }, None)
+        decision = self.switcher.decide({ device: { self.equihash: 2.0,
+                                                    self.neoscrypt: 2.5 } }, None)
+
+        self.assertEqual(decision[device], self.equihash)
+
+    def test_above_threshold(self):
+        device = self.devices[0]
+
+        self.switcher.decide({ device: { self.equihash: 2.0,
+                                         self.neoscrypt: 1.0 } }, None)
+        decision = self.switcher.decide({ device: { self.equihash: 2.0,
+                                                    self.neoscrypt: 3.5 } }, None)
+
+        self.assertEqual(decision[device], self.neoscrypt)
+
+if __name__ == '__main__':
+    main()
+
