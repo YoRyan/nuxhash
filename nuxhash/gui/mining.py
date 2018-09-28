7cf96910d542c8a523ab4ce2fca3fb6b36d8f484
Ryan Young <rayoung@utexas.edu>
2018-09-28 14:08:10 -0700

    Polish and custom device list renderer for the GUI

--- nuxhash/gui/mining.py
+++ nuxhash/gui/mining.py
@@ -64,9 +64,12 @@ class MiningScreen(wx.Panel):
 
     def start_mining(self):
         self._startstop.SetLabel('Stop Mining')
+        self._mining.start_mining()
 
     def stop_mining(self):
+        self._revenue.SetLabel('')
         self._startstop.SetLabel('Start Mining')
+        self._mining.stop_mining()
 
     def set_revenue(self, v):
         unit = self._settings['gui']['units']
@@ -94,18 +97,27 @@ class MiningPanel(wx.ScrolledCanvas):
         self.SetSizer(self._sizer)
 
         self._algorithms = wx.dataview.DataViewListCtrl(self)
-        self._algorithms.AppendTextColumn('Algorithm')
+        self._algorithms.Disable()
+        self._algorithms.AppendTextColumn('Algorithm', width=wx.COL_WIDTH_AUTOSIZE)
         self._algorithms.AppendColumn(
             wx.dataview.DataViewColumn('Devices', DeviceListRenderer(),
-                                       1, align=wx.ALIGN_LEFT),
+                                       1, align=wx.ALIGN_LEFT,
+                                       width=wx.COL_WIDTH_AUTOSIZE),
             'string')
-        self._algorithms.AppendTextColumn('Speed')
+        self._algorithms.AppendTextColumn('Speed', width=wx.COL_WIDTH_AUTOSIZE)
         self._algorithms.AppendTextColumn('Revenue')
         self._sizer.Add(self._algorithms, wx.SizerFlags().Proportion(1.0)
                                                          .Expand())
 
     def read_settings(self, new_settings):
         self._settings = new_settings
+
+    def start_mining(self):
+        self._algorithms.Enable()
+
+    def stop_mining(self):
+        self._algorithms.Disable()
+        self._algorithms.DeleteAllItems()
 
     def display_status(self,
                        speeds=defaultdict(lambda: [0.0]),
@@ -115,10 +127,11 @@ class MiningPanel(wx.ScrolledCanvas):
         algorithms = list(speeds.keys())
         algorithms.sort(key=lambda algorithm: algorithm.name)
         for algorithm in algorithms:
-            algo = '%s (%s)' % (algorithm.name, ', '.join(algorithm.algorithms))
+            algo = '%s\n(%s)' % (algorithm.name, ', '.join(algorithm.algorithms))
             devices = ','.join([DeviceListRenderer.device_to_string(device)
                                 for device in devices[algorithm]])
-            speed = utils.format_speeds(speeds[algorithm])
+            speed = ',\n'.join([utils.format_speed(speed)
+                                for speed in speeds[algorithm]])
             revenue = utils.format_balance(revenue[algorithm],
                                            self._settings['gui']['units'])
             self._algorithms.AppendItem([algo, devices, speed, revenue])
@@ -126,24 +139,63 @@ class MiningPanel(wx.ScrolledCanvas):
 
 class DeviceListRenderer(wx.dataview.DataViewCustomRenderer):
 
+    CORNER_RADIUS = 5
+
     def __init__(self, *args, **kwargs):
         wx.dataview.DataViewCustomRenderer.__init__(self, *args, **kwargs)
-        self.value = None
+        self.devices = []
+        self._colordb = wx.ColourDatabase()
 
     def SetValue(self, value):
-        self.value = value
+        vendors = {
+            'N': 'nvidia'
+            }
+        self.devices = [{ 'name': s[2:], 'vendor': vendors[s[0]] }
+                        for s in value.split(',')]
         return True
 
     def GetValue(self):
-        return self.value
+        tags = {
+            'nvidia': 'N'
+            }
+        return ','.join(['%s:%s' % (tags[device['vendor']], device['name'])
+                         for device in self.devices])
 
     def GetSize(self):
-        text = self.value.replace(',', '\n')
-        return self.GetTextExtent(text)
+        boxes = [self.GetTextExtent(device['name']) for device in self.devices]
+        return wx.Size((max(box.GetWidth() for box in boxes)
+                        + DeviceListRenderer.CORNER_RADIUS*2),
+                       (sum(box.GetHeight() for box in boxes)
+                        + DeviceListRenderer.CORNER_RADIUS*2*len(boxes)
+                        + DeviceListRenderer.CORNER_RADIUS*(len(boxes) - 1)))
 
     def Render(self, cell, dc, state):
-        text = self.value.replace(',', '\n')
-        self.RenderText(text, 0, cell, dc, state)
+        position = cell.GetPosition()
+        for device in self.devices:
+            box = self.GetTextExtent(device['name'])
+
+            if device['vendor'] == 'nvidia':
+                color = self._colordb.Find('LIME GREEN')
+            else:
+                color = self._colordb.Find('LIGHT GREY')
+            dc.SetBrush(wx.Brush(color))
+            dc.SetPen(wx.TRANSPARENT_PEN)
+            shade_rect = wx.Rect(
+                position,
+                wx.Size(box.GetWidth() + DeviceListRenderer.CORNER_RADIUS*2,
+                        box.GetHeight() + DeviceListRenderer.CORNER_RADIUS*2))
+            dc.DrawRoundedRectangle(shade_rect, DeviceListRenderer.CORNER_RADIUS)
+
+            text_rect = wx.Rect(
+                wx.Point(position.x + DeviceListRenderer.CORNER_RADIUS,
+                         position.y + DeviceListRenderer.CORNER_RADIUS),
+                box)
+            self.RenderText(device['name'], 0, text_rect, dc, state)
+
+            position = wx.Point(position.x,
+                                (position.y + box.GetHeight()
+                                 + DeviceListRenderer.CORNER_RADIUS*2
+                                 + DeviceListRenderer.CORNER_RADIUS))
         return True
 
     def device_to_string(device):
@@ -151,8 +203,9 @@ class DeviceListRenderer(wx.dataview.DataViewCustomRenderer):
             name = device.name
             name = name.replace('GeForce', '')
             name = name.replace('GTX', '')
+            name = name.replace('RTX', '')
             name = name.strip()
-            return 'NV:%s' % name
+            return 'N:%s' % name
         else:
             raise Exception('bad device instance')
 
