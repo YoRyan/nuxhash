33e9a095a41a6828959927f867efe4d2df398ede
Ryan Young <ryan@youngryan.com>
2019-10-12 22:10:40 -0700

    pip: include NiceHash REST client library

--- setup.py
+++ setup.py
@@ -134,7 +134,9 @@ setup(
     #
     #   py_modules=["my_module"],
     #
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']) + [
+        'nuxhash/nhrest/python'
+    ],  # Required
 
     # This field lists other packages that your project depends on to run.
     # Any package you put here will be installed by pip when your project is
